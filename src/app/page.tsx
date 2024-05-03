c498594fe88f627f9895c84bb63b2a2ed1ee895d
EllectronChik <electronchik04@mail.ru>
2024-05-03 21:10:36 +0300

    changed link to be dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { redirect } from "next/navigation";
 import classes from "./page.module.scss";
 import { cookies } from "next/headers";
 import GlitchedText from "@/components/GlitchedText/GlitchedText";
@@ -6,18 +5,17 @@ import Link from "next/link";
 import { oxta } from "@/lib/fonts";
 
 export default function Home() {
-  if (cookies().get("refresh-token")?.value) {
-    redirect("/diary");
-  } else {
-    return (
-      <main className={classes.container}>
-        <Link href="/login" className={classes.button}>
-          <GlitchedText
-            text="EllectronChik"
-            className={`${classes.title} ${oxta.className}`}
-          />
-        </Link>
-      </main>
-    );
-  }
+  return (
+    <main className={classes.container}>
+      <Link
+        href={cookies().get("logedIn")?.value ? "/diary" : "/login"}
+        className={classes.button}
+      >
+        <GlitchedText
+          text="EllectronChik"
+          className={`${classes.title} ${oxta.className}`}
+        />
+      </Link>
+    </main>
+  );
 }
