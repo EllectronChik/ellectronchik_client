ff3f2b091ae26f109069257540ca3e9f7c0dc4c2
EllectronChik <electronchik04@mail.ru>
2024-05-03 21:14:21 +0300

    added func that use fetched encrypted data to get readable text

--- src/app/(withlayout)/diary/page.tsx
+++ src/app/(withlayout)/diary/page.tsx
@@ -1,28 +1,48 @@
+import decryptText from "@/actions/decryptText";
+import DiaryList from "@/components/DiaryList/DiaryList";
 import { getClient } from "@/lib/graphql/client";
+import { IDiaryNote } from "@/models/IDiaryNote";
 import { gql } from "@apollo/client";
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+import classes from "./diary.module.scss";
+import ITag from "@/models/ITag";
+
+interface IDiaryData {
+  findUserNotesPaginated: IDiaryNote[];
+  findTagsByUser: ITag[];
+}
 
 const Diary = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) => {
-
-  const GQL = gql`
-  query FindUserNotesPaginated($page: Int, $limit: Int) {
-    findUserNotesPaginated(page: $page, limit: $limit) {
+  const NOTES_QUERY = gql`
+    query FindUserNotesPaginated(
+      $page: Float
+      $limit: Float
+      $direction: Float
+    ) {
+      findUserNotesPaginated(
+        page: $page
+        limit: $limit
+        direction: $direction
+      ) {
+        _id
         encryptedTitle
         encryptedText
         createdAt
         updatedAt
         tags
+        iv
+      }
+      findTagsByUser {
+        _id
+        name
+        color
+      }
     }
-}
-`
-  if (!cookies().get("refresh-token")?.value) {
-    redirect("/login");
-  }
+  `;
 
   const page = Number.isNaN(parseInt(searchParams?.page || "1", 10))
     ? 1
@@ -30,29 +50,72 @@ const Diary = async ({
   const limit = Number.isNaN(parseInt(searchParams?.limit || "10", 10))
     ? 10
     : parseInt(searchParams?.limit || "10", 10);
+  const direction = searchParams?.direction === "-1" ? -1 : 1;
 
-
-
-  const { data, error } = await getClient().query({
-    query: GQL,
-    variables: {
-      page,
-      limit
-    },
-    context: {
-      headers: {
-        "cookie": searchParams?.cookie || "",
+  const { data, error } = await getClient()
+    .query<IDiaryData>({
+      query: NOTES_QUERY,
+      variables: {
+        page,
+        limit,
+        direction,
       },
-    },
-    errorPolicy: "all",
-  });
+      context: {
+        headers: {
+          cookie:
+            cookies()
+              .getAll()
+              .map(({ name, value }) => `${name}=${value}`)
+              .join(";") || "",
+        },
+      },
+      errorPolicy: "all",
+    })
+    .then(async (res) => {
+      if (!res.data?.findUserNotesPaginated) {
+        return { data: res.data, error: res.error };
+      }
+      const decryptedNotes: IDiaryNote[] = await Promise.all(
+        res.data?.findUserNotesPaginated?.map(async (note) => {
+          let decryptedData;
+          try {
+            decryptedData = await decryptText(
+              note.encryptedTitle,
+              note.encryptedText,
+              note.iv
+            );
+          } catch (error) {
+            console.error(error);
+            return note;
+          }
+          const decryptedNote: IDiaryNote = {
+            ...note,
+            encryptedTitle: decryptedData.title,
+            encryptedText: decryptedData.text,
+          };
+
+          return decryptedNote;
+        })
+      );
+      const responseData: IDiaryData = {
+        findUserNotesPaginated: decryptedNotes,
+        findTagsByUser: res.data?.findTagsByUser || [],
+      };
+      return { data: responseData, error: res.error };
+    });
 
   if (error) {
-    console.error(error);
-    return null;
+    return <div>{error.message}</div>;
   }
 
-  return <div></div>;
+  return (
+    <div className={classes.container}>
+      <DiaryList
+        notes={data?.findUserNotesPaginated || []}
+        tags={data?.findTagsByUser || []}
+      />
+    </div>
+  );
 };
 
 export default Diary;
