8a35a52d36c119ef3aae7b26f921499950564c77
ellectronchik <electronchik04@mail.ru>
2024-05-12 22:18:10 +0300

    added redirect to note page

--- src/components/DiaryList/DiaryList.tsx
+++ src/components/DiaryList/DiaryList.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { FC, HTMLProps, useState } from "react";
 import classes from "./DiaryList.module.scss";
@@ -6,6 +6,7 @@ import DiaryItem from "../DiaryItem/DiaryItem";
 import { IDiaryNote } from "@/models/IDiaryNote";
 import ITag from "@/models/ITag";
 import SettingsBar from "../SettingsBar/SettingsBar";
+import { useRouter } from "next/navigation";
 
 interface IDiaryListProps extends HTMLProps<HTMLDivElement> {
   notes: IDiaryNote[];
@@ -13,15 +14,24 @@ interface IDiaryListProps extends HTMLProps<HTMLDivElement> {
 }
 
 const DiaryList: FC<IDiaryListProps> = ({ notes, tags, ...props }) => {
-
   const [notesData, setNotesData] = useState(notes);
+  const router = useRouter();
+
+  const handleNoteClick = (note: IDiaryNote) => {
+    router.push(`/diary/${note._id}`);
+  };
 
   return (
     <div {...props} className={`${props.className} ${classes.container}`}>
       <SettingsBar setNotes={setNotesData} />
       <div className={classes.list}>
         {notesData.map((note) => (
-          <DiaryItem key={note._id} note={note} tags={tags} />
+          <DiaryItem
+            onClick={() => handleNoteClick(note)}
+            key={note._id}
+            note={note}
+            tags={tags}
+          />
         ))}
       </div>
     </div>
