d5715723fdbabedeb85ea6697eef28bfd1dc95a4
ellectronchik <electronchik04@mail.ru>
2024-05-12 22:22:59 +0300

    added clickable tag variant

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -4,13 +4,23 @@ import classes from "./Tag.module.scss";
 interface ITagProps extends HTMLProps<HTMLDivElement> {
   name: string;
   color: string;
+  canClick?: boolean;
 }
 
-const Tag: FC<ITagProps> = ({ name, color, ...props }) => {
+const Tag: FC<ITagProps> = ({ name, color, canClick = false, ...props }) => {
   return (
-    <div style={{ borderColor: color }} {...props} className={`${classes.container} ${props.className}`}>
+    <div
+      style={{ borderColor: color }}
+      {...props}
+      className={`${classes.container} ${props.className} ${
+        canClick ? classes.clickable : ""
+      }`}
+    >
       <p className={classes.name}>{name}</p>
-      <span className={classes.color} style={{ backgroundColor: color }} />
+      <span
+        className={`${classes.color} ${canClick ? classes.clickable : ""}`}
+        style={{ backgroundColor: color }}
+      />
     </div>
   );
 };
