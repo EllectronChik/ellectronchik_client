447c5943b201807ad6bee4311e7f10122c68fd0c
ellectronchik <electronchik04@mail.ru>
2024-05-12 22:05:22 +0300

    created note creation page

---
+++ src/app/(withlayout)/diary/create/page.tsx
@@ -0,0 +1,46 @@
+import { getClient } from "@/lib/graphql/client";
+import classes from "./createnote.module.scss";
+import { gql } from "@apollo/client";
+import { cookies } from "next/headers";
+import ITag from "@/models/ITag";
+import CreateNoteCard from "@/components/CreateNoteCard/CreateNoteCard";
+
+interface IDiaryData {
+  findTagsByUser: ITag[];
+}
+
+const CreateNote = async () => {
+  const GET_TAGS = gql`
+    query FindTagsByUser {
+      findTagsByUser {
+        _id
+        name
+        color
+      }
+    }
+  `;
+
+  const { data: tags, loading, error } = await getClient().query<IDiaryData>({
+    query: GET_TAGS,
+    context: {
+      headers: {
+        cookie:
+          cookies()
+            .getAll()
+            .map(({ name, value }) => `${name}=${value}`)
+            .join(";") || "",
+      },
+    },
+    errorPolicy: "all",
+  });
+
+  return (
+    <div className={classes.container}>
+      {tags && <CreateNoteCard tags={tags.findTagsByUser} />}
+      {loading && <div>Loading</div>}
+      {error && <div>Error</div>}
+    </div>
+  );
+};
+
+export default CreateNote;
