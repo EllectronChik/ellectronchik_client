fd26fb642364a868a95e6dd55dd43bc60fa20d5a
ellectronchik <electronchik04@mail.ru>
2024-05-12 22:02:44 +0300

    changed iv be arg

--- src/actions/encryptText.ts
+++ src/actions/encryptText.ts
@@ -4,18 +4,24 @@ import { cookies } from "next/headers";
 
 export default async function encryptText(
   title: string,
-  text: string
+  text: string,
+  iv: string
 ) {
-  const secret = cookies().get("key")?.value || '';
-  const iv = crypto.randomBytes(16);
-  const cipherTitle = crypto.createCipheriv("aes-256-cbc", secret, iv);
-  const cipherText = crypto.createCipheriv("aes-256-cbc", secret, iv);
-  const encryptedTitle = Buffer.concat([cipherTitle.update(title), cipherTitle.final()]);
-  const encryptedText = Buffer.concat([cipherText.update(text), cipherText.final()]);
+  const ivBuffer = Buffer.from(iv, "hex");
+  const secret = cookies().get("key")?.value || "";
+  const cipherTitle = crypto.createCipheriv("aes-256-cbc", secret, ivBuffer);
+  const cipherText = crypto.createCipheriv("aes-256-cbc", secret, ivBuffer);
+  const encryptedTitle = Buffer.concat([
+    cipherTitle.update(title),
+    cipherTitle.final(),
+  ]);
+  const encryptedText = Buffer.concat([
+    cipherText.update(text),
+    cipherText.final(),
+  ]);
 
   return {
     title: encryptedTitle.toString("hex"),
     text: encryptedText.toString("hex"),
-    iv: iv.toString("hex"),
   };
 }
