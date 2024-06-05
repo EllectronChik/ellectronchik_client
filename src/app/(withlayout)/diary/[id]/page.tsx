616420641e6571ba9a0df875874e98c3046875d9
ellectronchik <electronchik04@mail.ru>
2024-06-05 16:31:48 +0300

    added delete note btn + metadata

--- src/app/(withlayout)/diary/[id]/page.tsx
+++ src/app/(withlayout)/diary/[id]/page.tsx
@@ -7,12 +7,15 @@ import classes from "./diaryNote.module.scss";
 import CreateNote from "@/assets/images/create-note.svg";
 import Image from "next/image";
 import Link from "next/link";
+import DeleteDiaryItem from "@/components/DeleteDiaryItem/DeleteDiaryItem";
+import { Metadata } from "next";
+import LoadImage from "@/components/LoadImage/loadImage";
 
 interface IDiaryData {
   findNoteById: {
     encryptedTitle: string;
     encryptedText: string;
-    encryptedMedia: string;
+    diaryNoteMedia: { mediaPath: string; mediaIVHex: string }[];
     createdAt: string;
     updatedAt: string;
     tags: string[];
@@ -30,13 +33,16 @@ interface IVariables {
   id: string;
 }
 
-const DiaryNote = async ({ params }: { params: { id: string } }) => {
+const fetchData = async (id: string) => {
   const noteQuery = gql`
     query FindNoteById($id: String!) {
       findNoteById(id: $id) {
         encryptedTitle
         encryptedText
-        encryptedMedia
+        diaryNoteMedia {
+          mediaPath
+          mediaIVHex
+        }
         createdAt
         updatedAt
         tags
@@ -50,13 +56,13 @@ const DiaryNote = async ({ params }: { params: { id: string } }) => {
       }
     }
   `;
-
-  const { data, error } = await getClient()
+  const { data, loading, errors } = await getClient()
     .query<IDiaryData, IVariables>({
       query: noteQuery,
       variables: {
-        id: params.id,
+        id: id,
       },
+      errorPolicy: "all",
       context: {
         headers: {
           cookie:
@@ -68,27 +74,60 @@ const DiaryNote = async ({ params }: { params: { id: string } }) => {
       },
     })
     .then(async (res) => {
-      const decrypted = await decryptText(
-        res.data?.findNoteById.encryptedTitle,
-        res.data?.findNoteById.encryptedText,
-        res.data?.findNoteById.iv
-      );
-      return {
-        data: {
-          findNotesById: {
-            ...res.data?.findNoteById,
-            ...decrypted,
+      try {
+        const decrypted = await decryptText(
+          res.data?.findNoteById.encryptedTitle,
+          res.data?.findNoteById.encryptedText,
+          res.data?.findNoteById.iv
+        );
+        return {
+          data: {
+            findNotesById: {
+              ...res.data?.findNoteById,
+              ...decrypted,
+            },
+            findTagsByUser: res.data?.findTagsByUser || [],
           },
-          findTagsByUser: res.data?.findTagsByUser || [],
-        },
-        loading: res.loading,
-        error: res.error,
-      };
+          loading: res.loading,
+          errors: res.errors,
+        };
+      } catch (error) {
+        return {
+          data: null,
+          loading: res.loading,
+          errors: res.errors,
+        };
+      }
     });
+  return {
+    data: data,
+    loading: loading,
+    errors: errors,
+  };
+};
+
+export const generateMetadata = async (props: {
+  params: { id: string };
+}): Promise<Metadata> => {
+  const { data } = await fetchData(props.params.id);
+  return {
+    title: data?.findNotesById.title,
+    description: `Page with diary note: ${data?.findNotesById.title}`,
+  };
+};
+
+const DiaryNote = async ({ params }: { params: { id: string } }) => {
+  const { data, errors } = await fetchData(params.id);
 
   return (
     <>
-      {error && <p>{error.message}</p>}
+      {errors && (
+        <div className={classes.container}>
+          <p className={classes.error}>
+            {errors.map((error) => error.message)}
+          </p>
+        </div>
+      )}
       {data && (
         <div className={classes.container}>
           <div className={classes.content}>
@@ -99,10 +138,12 @@ const DiaryNote = async ({ params }: { params: { id: string } }) => {
                   Created at:{" "}
                   {new Date(data.findNotesById.createdAt).toLocaleString()}
                 </p>
-                <p className={classes.date}>
-                  Updated at:{" "}
-                  {new Date(data.findNotesById.updatedAt).toLocaleString()}
-                </p>
+                {data.findNotesById.updatedAt && (
+                  <p className={classes.date}>
+                    Updated at:{" "}
+                    {new Date(data.findNotesById.updatedAt).toLocaleString()}
+                  </p>
+                )}
               </div>
               <div className={classes.tags}>
                 {data.findNotesById.tags.map((tag) => {
@@ -119,14 +160,27 @@ const DiaryNote = async ({ params }: { params: { id: string } }) => {
                 })}
               </div>
             </div>
-            <p className={classes.text}>{data.findNotesById.text}</p>
-            <Link href={`/diary/update/${params.id}`} className={classes.link}>
-              <Image
-                src={CreateNote}
-                alt="create-note"
-                className={classes.createNote}
-              />
-            </Link>
+            <div className={classes.contantContainer}>
+              <p>{data.findNotesById.text}</p>
+              {data.findNotesById.diaryNoteMedia.length > 0 && (
+                <div className={classes.images}>
+                  {data.findNotesById.diaryNoteMedia.map((media) => <LoadImage key={media.mediaIVHex} media={media} className={classes.image} />)}
+                </div>
+              )}
+            </div>
+            <div className={classes.actions}>
+              <Link
+                href={`/diary/update/${params.id}`}
+                className={classes.link}
+              >
+                <Image
+                  src={CreateNote}
+                  alt="create-note"
+                  className={classes.createNote}
+                />
+              </Link>
+              <DeleteDiaryItem id={params.id} />
+            </div>
           </div>
         </div>
       )}
