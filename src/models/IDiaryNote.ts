52107874c617c3b35763f1224bbeb671b0ca4db0
ellectronchik <electronchik04@mail.ru>
2024-06-05 16:26:14 +0300

    added diaryNoteMedia

--- src/models/IDiaryNote.ts
+++ src/models/IDiaryNote.ts
@@ -2,6 +2,7 @@ export interface IDiaryNote {
   _id: string;
   encryptedTitle: string;
   encryptedText: string;
+  diaryNoteMedia: { mediaPath: string; mediaIVHex: string }[];
   createdAt: Date;
   updatedAt: Date;
   tags: string[];
