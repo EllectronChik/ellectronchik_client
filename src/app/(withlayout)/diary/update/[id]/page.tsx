3077a2b85eb4dbc5b807fb05a42811472845d431
ellectronchik <electronchik04@mail.ru>
2024-06-05 16:32:03 +0300

    added metadata

--- src/app/(withlayout)/diary/update/[id]/page.tsx
+++ src/app/(withlayout)/diary/update/[id]/page.tsx
@@ -7,13 +7,14 @@ import CreateNoteCard from "@/components/CreateNoteCard/CreateNoteCard";
 import { IDiaryNote } from "@/models/IDiaryNote";
 import decryptText from "@/actions/decryptText";
 import { IDiaryNoteDecrypted } from "@/models/IDiaryNoteDecrypted";
+import { Metadata } from "next";
 
 interface IDiaryData {
   findTagsByUser: ITag[];
   findNoteById: IDiaryNote;
 }
 
-const UpdateNote = async ({ params }: { params: { id: string } }) => {
+const fetchData = async (id: string) => {
   const GET_TAGS = gql`
     query FindTagsByUser($id: String!) {
       findTagsByUser {
@@ -24,7 +25,10 @@ const UpdateNote = async ({ params }: { params: { id: string } }) => {
       findNoteById(id: $id) {
         encryptedTitle
         encryptedText
-        encryptedMedia
+        diaryNoteMedia {
+          mediaPath
+          mediaIVHex
+        }
         tags
         iv
         _id
@@ -34,13 +38,13 @@ const UpdateNote = async ({ params }: { params: { id: string } }) => {
 
   const {
     data: tags,
+    errors,
     loading,
-    error,
   } = await getClient()
     .query<IDiaryData>({
       query: GET_TAGS,
       variables: {
-        id: params.id,
+        id: id,
       },
       context: {
         headers: {
@@ -54,23 +58,59 @@ const UpdateNote = async ({ params }: { params: { id: string } }) => {
       errorPolicy: "all",
     })
     .then(async (res) => {
-      const decrypted = await decryptText(
-        res.data.findNoteById.encryptedTitle,
-        res.data.findNoteById.encryptedText,
-        res.data.findNoteById.iv
-      );
-      const findNoteById: IDiaryNoteDecrypted = {
-        ...res.data.findNoteById,
-        ...decrypted,
-      };
-      return {
-        ...res,
-        data: {
-          ...res.data,
-          findNoteById,
-        },
-      };
+      try {
+        const decrypted = await decryptText(
+          res.data.findNoteById.encryptedTitle,
+          res.data.findNoteById.encryptedText,
+          res.data.findNoteById.iv
+        );
+        const findNoteById: IDiaryNoteDecrypted = {
+          ...res.data.findNoteById,
+          ...decrypted,
+        };
+        return {
+          ...res,
+          data: {
+            ...res.data,
+            findNoteById,
+          },
+        };
+      } catch (error) {
+        return {
+          ...res,
+          data: null,
+        };
+      }
     });
+
+  return {
+    tags,
+    errors,
+    loading,
+  };
+};
+
+export const generateMetadata = async (props: {
+  params: { id: string };
+}): Promise<Metadata> => {
+  const { tags, errors } = await fetchData(props.params.id);
+
+  if (errors) {
+    return {
+      title: "Error",
+      description: "Error",
+    };
+  }
+  return {
+    title: `Update Note - ${tags?.findNoteById.title || ""}`,
+    description: `Page with ${
+      tags?.findNoteById.title || ""
+    } diary note update`,
+  };
+};
+
+const UpdateNote = async ({ params }: { params: { id: string } }) => {
+  const { tags, errors, loading } = await fetchData(params.id);
 
   return (
     <div className={classes.container}>
@@ -78,7 +118,7 @@ const UpdateNote = async ({ params }: { params: { id: string } }) => {
         <CreateNoteCard tags={tags.findTagsByUser} note={tags.findNoteById} />
       )}
       {loading && <div>Loading</div>}
-      {error && <div>Error</div>}
+      {errors && <div>Note not found</div>}
     </div>
   );
 };
