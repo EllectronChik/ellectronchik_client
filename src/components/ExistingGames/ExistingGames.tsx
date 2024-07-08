6f2d7178999ff60bcca24410d884d33fdcd4d1e9
ellectronchik <electronchik04@mail.ru>
2024-07-08 19:10:23 +0300

    changed handleSelectGame to set name into cookie

--- src/components/ExistingGames/ExistingGames.tsx
+++ src/components/ExistingGames/ExistingGames.tsx
@@ -4,32 +4,40 @@ import { IGetGamesSubscriptionResp } from "@/models/IGetGamesSubscriptionResp";
 import { getGamesSubscription } from "@/queries/getGamesSubscription";
 import { useSubscription } from "@apollo/client";
 import classes from "./ExistingGames.module.scss";
-import { FC, HTMLProps } from "react";
+import { FC, HTMLProps, MouseEvent } from "react";
 import GameInfo from "../GameInfo/GameInfo";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
+import getCookie from "@/lib/client/getCookie";
+import setCookie from "@/lib/client/setCookie";
 
 interface IProps extends HTMLProps<HTMLDivElement> {
   savePack: boolean;
   isCustomWordlist: boolean;
+  playerName: string;
+  playerAvatarId: number;
 }
 
-const ExistingGame: FC<IProps> = ({ savePack, isCustomWordlist, ...props }) => {
+const ExistingGame: FC<IProps> = ({ savePack, isCustomWordlist, playerName, playerAvatarId, ...props }) => {
   const { data, loading, error } =
     useSubscription<IGetGamesSubscriptionResp>(getGamesSubscription);
 
   const router = useRouter();
 
-  const handleSelectGame = (id: string) => {
+  const handleSelectGame = (e: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>, id: string) => {
+    e.preventDefault();
+    const savedPlayerName = getCookie("playerName");
+    const savedPlayerAvatarId = getCookie("playerAvatarId");
+    
+    if (!savedPlayerName) {
+      setCookie("playerName", playerName, 1000 * 24 * 60 * 60 * 31);
+    }
+
+    if (!savedPlayerAvatarId) {
+      setCookie("playerAvatarId", playerAvatarId.toString(), 1000 * 24 * 60 * 60 * 31);
+    }
     router.push(`/drawit/${id}`);
   };
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error</div>;
-  }
 
   return (
     <div
@@ -40,16 +48,26 @@ const ExistingGame: FC<IProps> = ({ savePack, isCustomWordlist, ...props }) => {
       }`}
     >
       <h3 className={classes.title}>Join an existing game</h3>
-      {!data?.getGames?.length && <p className={classes.noGames}>No games</p>}
-      <div className={classes.games}>
-        {data?.getGames?.map((game) => (
-          <GameInfo
-            key={game.id}
-            game={game}
-            onClick={() => handleSelectGame(game.id)}
-          />
-        ))}
-      </div>
+      {!data?.getGames?.length && !loading && !error && (
+        <p className={classes.noGames}>No games</p>
+      )}
+      {loading && <p className={classes.noGames}>Loading...</p>}
+      {error && <p className={classes.noGames}>Error</p>}
+      {data && (
+        <div className={classes.games}>
+          {data?.getGames?.map((game) => (
+            <Link
+              className={classes.game}
+              key={game.id}
+              href={`/drawit/${game.id}`}
+              onClick={(e) => handleSelectGame(e, game.id)}
+            >
+              <GameInfo
+              game={game}
+            /></Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
