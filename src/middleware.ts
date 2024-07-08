d6282cd5afe5dae497ef6573c4f54b44a5319623
ellectronchik <electronchik04@mail.ru>
2024-07-08 18:11:52 +0300

    new middleware to validate drawit player data

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,12 @@
 import { NextRequest } from "next/server";
 import { revalidateMiddleware } from "./middlewares/revalidateMiddleware";
+import { valdateDrawitPlayerData } from "./middlewares/valdateDrawitPlayerData";
 
 export const middleware = async (request: NextRequest) => {
   if (request.nextUrl.pathname.startsWith("/diary")) {
     return revalidateMiddleware(request);
   }
-}
+  if (request.nextUrl.pathname.startsWith("/drawit")) {
+    return valdateDrawitPlayerData(request);
+  }
+};
